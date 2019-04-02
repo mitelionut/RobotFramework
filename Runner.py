f9814c09c9fc24f1b7dff2e80da9ddccd7ce098e
Ionel Mitel <mitelionut@yahoo.com>
2019-04-02 15:48:44 +0300

    Final version of runner

--- Runner.py
+++ Runner.py
@@ -3,9 +3,11 @@ import os
 
 timestamp = time.strftime("%Y%m%d-%H%M%S")
 log_file = "log_webdemo_" + timestamp + ".html"
-report_file = "report_webdemo" + timestamp + "html"
+report_file = "report_webdemo" + timestamp + ".html"
 output_file = "output_webdemo" + timestamp + ".xml"
+html_tag1 = "Application_Name:WebDemoOfTheMillenium"
+html_tag2 = "Version:1.0"
 
-comanda = "robot -r " + report_file + " -l " + log_file + " login_tests"
+comanda = "robot -r " + report_file + " -l " + log_file + " -M " + html_tag1 + " -M " + html_tag2 +" login_tests"
 
 os.system(comanda)
